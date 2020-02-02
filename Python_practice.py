2bbe1378c94f8da99daa80d090e5672936e5e33b
Sam Ben <samben@Sams-MacBook-Air.local>
2020-02-01 18:22:25 -0800

    Adding the election audit code.

---
+++ Python_practice.py
@@ -0,0 +1,13 @@
+
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+
+
+
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+print("hello world")
+print(9+7)
+
+
